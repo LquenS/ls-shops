b080b2fcdd14e2888d65ac0c9c9eebd8b4afdd7f
LquenS <atakan.ts6161@gmail.com>
2023-08-13 10:07:32 +0300

    Some Modification
    Locales totally removed. Adapted work with ls-core instead "qb-core".

--- client/main.lua
+++ client/main.lua
@@ -1,20 +1,45 @@
-local QBCore = exports["qb-core"]:GetCoreObject()
-local PlayerData = QBCore.Functions.GetPlayerData()
+local LS_CORE = exports["ls-core"]:GetCoreObject()
 local inChips = false
 local currentShop, currentData
 local pedSpawned = false
-local listen = false
 local ShopPed = {}
-local NewZones = {}
--- Functions
-local function createBlips()
-    if pedSpawned then return end
 
-    for store in pairs(Config.Locations) do
+local function GetPlayerJob()
+    return LS_CORE.Functions.GetPlayerData().PlayerData.job.name
+end
+
+local function ShowHelpText(string)	
+    BeginTextCommandDisplayHelp("STRING")
+	AddTextComponentSubstringPlayerName(string)
+    EndTextCommandDisplayHelp(0, 0, 1, -1)
+end
+
+
+-- Functions
+local function SetupItems(shop)
+    local products = Config.Locations[shop].products
+    local playerJob = GetPlayerJob()
+    local items = {}
+    for i = 1, #products do
+        if not products[i].requiredJob then
+            items[#items + 1] = products[i]
+        else
+            for i2 = 1, #products[i].requiredJob do
+                if playerJob == products[i].requiredJob[i2] then
+                    items[#items + 1] = products[i]
+                end
+            end
+        end
+    end
+    return items
+end
+
+local function createBlips()
+    for store, _ in pairs(Config.Locations) do
         if Config.Locations[store]["showblip"] then
             local StoreBlip = AddBlipForCoord(Config.Locations[store]["coords"]["x"], Config.Locations[store]["coords"]["y"], Config.Locations[store]["coords"]["z"])
             SetBlipSprite(StoreBlip, Config.Locations[store]["blipsprite"])
-            SetBlipScale(StoreBlip, Config.Locations[store]["blipscale"])
+            SetBlipScale(StoreBlip, 0.6)
             SetBlipDisplay(StoreBlip, 4)
             SetBlipColour(StoreBlip, Config.Locations[store]["blipcolor"])
             SetBlipAsShortRange(StoreBlip, true)
@@ -23,175 +48,6 @@ local function createBlips()
             EndTextCommandSetBlipName(StoreBlip)
         end
     end
-end
-
-local function openShop(shop, data)
-    QBCore.Functions.TriggerCallback('qb-shops:server:SetShopInv', function(shopInvJson)
-        local function SetupItems(checkLicense)
-            local products =  Config.Locations[shop].products
-            local items = {}
-            local curJob
-            local curGang
-            shopInvJson = json.decode(shopInvJson)
-            if Config.UseTruckerJob and next(shopInvJson) and shopInvJson[shop] then
-                if next(shopInvJson) then
-                    for k, v in pairs(shopInvJson[shop].products) do
-                        products[k].amount = v.amount
-                    end
-                else print('No shop inventory found -- defaults enabled') end
-            end
-            for i = 1, #products do
-            curJob = products[i].requiredJob
-            curGang = products[i].requiredGang
-            if curJob then goto jobCheck end
-            if curGang then goto gangCheck end
-            if checkLicense then goto licenseCheck end
-            items[#items + 1] = products[i]
-            goto nextIteration
-            :: jobCheck ::
-            for i2 = 1, #curJob do
-                if PlayerData.job.name == curJob[i2] then
-                    items[#items + 1] = products[i]
-                end
-            end
-            goto nextIteration
-            :: gangCheck ::
-            for i2 = 1, #curGang do
-                if PlayerData.gang.name == curGang[i2] then
-                    items[#items + 1] = products[i]
-                end
-            end
-            goto nextIteration
-            :: licenseCheck ::
-            if not products[i].requiresLicense then
-                items[#items + 1] = products[i]
-            end
-            :: nextIteration ::
-            end
-            return items
-        end
-        TriggerServerEvent('qb-shops:server:SetShopList')
-        local ShopItems = {}
-        ShopItems.items = {}
-        ShopItems.label = data["label"]
-        if data.type == "weapon" and Config.FirearmsLicenseCheck then
-            if PlayerData.metadata["licences"] and PlayerData.metadata["licences"].weapon and QBCore.Functions.HasItem("weaponlicense") then
-                ShopItems.items = SetupItems()
-                QBCore.Functions.Notify(Lang:t("success.dealer_verify"), "success")
-                Wait(500)
-            else
-                ShopItems.items = SetupItems(true)
-                QBCore.Functions.Notify(Lang:t("error.dealer_decline"), "error")
-                Wait(500)
-                QBCore.Functions.Notify(Lang:t("error.talk_cop"), "error")
-                Wait(1000)
-            end
-        else
-            ShopItems.items = SetupItems()
-        end
-
-        for k in pairs(ShopItems.items) do
-            ShopItems.items[k].slot = k
-        end
-        TriggerServerEvent("inventory:server:OpenInventory", "shop", "Itemshop_" .. shop, ShopItems)
-    end)
-end
-
-local function listenForControl()
-    if listen then return end
-    CreateThread(function()
-        listen = true
-        while listen do
-            if IsControlJustPressed(0, 38) then -- E
-                TriggerServerEvent('qb-shops:server:SetShopList')
-                if inChips then
-                    exports["qb-core"]:KeyPressed()
-                    TriggerServerEvent("qb-shops:server:sellChips")
-                else
-                    exports["qb-core"]:KeyPressed()
-                    openShop(currentShop, currentData)
-                end
-                listen = false
-                break
-            end
-            Wait(0)
-        end
-    end)
-end
-
-local function createPeds()
-    if pedSpawned then return end
-
-    for k, v in pairs(Config.Locations) do
-        local current = type(v["ped"]) == "number" and v["ped"] or joaat(v["ped"])
-
-        RequestModel(current)
-        while not HasModelLoaded(current) do
-            Wait(0)
-        end
-
-        ShopPed[k] = CreatePed(0, current, v["coords"].x, v["coords"].y, v["coords"].z-1, v["coords"].w, false, false)
-        TaskStartScenarioInPlace(ShopPed[k], v["scenario"], 0, true)
-        FreezeEntityPosition(ShopPed[k], true)
-        SetEntityInvincible(ShopPed[k], true)
-        SetBlockingOfNonTemporaryEvents(ShopPed[k], true)
-
-        if Config.UseTarget then
-            exports['qb-target']:AddTargetEntity(ShopPed[k], {
-                options = {
-                    {
-                        label = v["targetLabel"],
-                        icon = v["targetIcon"],
-                        item = v["item"],
-                        action = function()
-                            openShop(k, Config.Locations[k])
-                        end,
-                        job = v.requiredJob,
-                        gang = v.requiredGang
-                    }
-                },
-                distance = 2.0
-            })
-        end
-    end
-
-    local current = type(Config.SellCasinoChips.ped) == 'number' and Config.SellCasinoChips.ped or joaat(Config.SellCasinoChips.ped)
-
-    RequestModel(current)
-    while not HasModelLoaded(current) do
-        Wait(0)
-    end
-
-    ShopPed["casino"] = CreatePed(0, current, Config.SellCasinoChips.coords.x, Config.SellCasinoChips.coords.y, Config.SellCasinoChips.coords.z-1, Config.SellCasinoChips.coords.w, false, false)
-    FreezeEntityPosition(ShopPed["casino"], true)
-    SetEntityInvincible(ShopPed["casino"], true)
-    SetBlockingOfNonTemporaryEvents(ShopPed["casino"], true)
-
-    if Config.UseTarget then
-        exports['qb-target']:AddTargetEntity(ShopPed["casino"], {
-            options = {
-                {
-                    label = 'Sell Chips',
-                    icon = 'fa-solid fa-coins',
-                    action = function()
-                        TriggerServerEvent("qb-shops:server:sellChips")
-                    end
-                }
-            },
-            distance = 2.0
-        })
-    end
-
-    pedSpawned = true
-end
-
-local function deletePeds()
-    if not pedSpawned then return end
-
-    for _, v in pairs(ShopPed) do
-        DeletePed(v)
-    end
-    pedSpawned = false
 end
 
 -- Events
@@ -204,91 +60,169 @@ RegisterNetEvent("qb-shops:client:SetShopItems", function(shop, shopProducts)
 end)
 
 RegisterNetEvent("qb-shops:client:RestockShopItems", function(shop, amount)
-    if not Config.Locations[shop].products then return end
-    for k in pairs(Config.Locations[shop].products) do
-        Config.Locations[shop].products[k].amount = Config.Locations[shop]["products"][k].amount + amount
+    if Config.Locations[shop]["products"] ~= nil then
+        for k in pairs(Config.Locations[shop]["products"]) do
+            Config.Locations[shop]["products"][k].amount = Config.Locations[shop]["products"][k].amount + amount
+        end
+    end
+end)
+
+local function openShop(shop, data)
+    local products = data.products
+    local ShopItems = {}
+    ShopItems.items = {}
+    LS_CORE.Callback.Functions.TriggerCallback("qb-shops:server:getLicenseStatus", function(hasLicense, hasLicenseItem)
+        ShopItems.label = data["label"]
+        if data.type == "weapon" then
+            if  hasLicenseItem then
+                ShopItems.items = SetupItems(shop)
+                Wait(500)
+            else
+                for i = 1, #products do
+                    if not products[i].requiredJob then
+                        if not products[i].requiresLicense then
+                            ShopItems.items[#ShopItems.items + 1] = products[i]
+                        end
+                    else
+                        for i2 = 1, #products[i].requiredJob do
+                            if GetPlayerJob() == products[i].requiredJob[i2] and not products[i].requiresLicense then
+                                ShopItems.items[#ShopItems.items + 1] = products[i]
+                            end
+                        end
+                    end
+                end
+                Wait(1000)
+            end
+        else
+            ShopItems.items = SetupItems(shop)
+        end
+        for k in pairs(ShopItems.items) do
+            ShopItems.items[k].slot = k
+        end
+        ShopItems.slots = 30
+        TriggerServerEvent("inventory:server:OpenInventory", "shop", "itemshop_" .. shop, ShopItems)
+    end)
+end
+
+local listen = false
+local function Listen4Control()
+    CreateThread(function()
+        listen = true
+        while listen do
+            ShowHelpText("Press [E] key to open shop") 
+            if IsControlJustPressed(0, 38) then -- E
+                if inChips then
+                    TriggerServerEvent("qb-shops:server:sellChips")
+                else
+                    openShop(currentShop, currentData)
+                end
+                listen = false
+                break
+            end
+            Wait(1)
+        end
+    end)
+end
+
+local function createPeds()
+    if pedSpawned then return end
+    for k, v in pairs(Config.Locations) do
+        if not ShopPed[k] then ShopPed[k] = {} end
+        local current = v["ped"]
+        current = type(current) == 'string' and GetHashKey(current) or current
+        RequestModel(current)
+
+        while not HasModelLoaded(current) do
+            Wait(0)
+        end
+        ShopPed[k] = CreatePed(0, current, v["coords"].x, v["coords"].y, v["coords"].z-1, v["coords"].w, false, false)
+        TaskStartScenarioInPlace(ShopPed[k], v["scenario"], true)
+        FreezeEntityPosition(ShopPed[k], true)
+        SetEntityInvincible(ShopPed[k], true)
+        SetBlockingOfNonTemporaryEvents(ShopPed[k], true)
+    end
+
+    if not ShopPed["casino"] then ShopPed["casino"] = {} end
+    local current = Config.SellCasinoChips.ped
+    current = type(current) == 'string' and GetHashKey(current) or current
+    RequestModel(current)
+
+    while not HasModelLoaded(current) do
+        Wait(0)
+    end
+    ShopPed["casino"] = CreatePed(0, current, Config.SellCasinoChips.coords.x, Config.SellCasinoChips.coords.y, Config.SellCasinoChips.coords.z-1, Config.SellCasinoChips.coords.w, false, false)
+    FreezeEntityPosition(ShopPed["casino"], true)
+    SetEntityInvincible(ShopPed["casino"], true)
+    SetBlockingOfNonTemporaryEvents(ShopPed["casino"], true)
+
+    pedSpawned = true
+end
+
+local function deletePeds()
+    if pedSpawned then
+        for _, v in pairs(ShopPed) do
+            DeletePed(v)
+        end
+    end
+end
+
+-- Threads
+
+local NewZones = {}
+CreateThread(function()
+    while true do
+        local sleep = 500
+        local playerCoords = GetEntityCoords(PlayerPedId())
+
+        for k in pairs( Config.Locations ) do
+            local shopCoords = vector3(Config.Locations[k]["coords"]["x"], Config.Locations[k]["coords"]["y"], Config.Locations[k]["coords"]["z"])
+
+            local distance = #(playerCoords - shopCoords)
+            if distance <= Config.Locations[k]["radius"] and not listen then
+                currentShop = k
+                currentData = Config.Locations[k]
+                Listen4Control()
+                sleep = 5
+            end
+
+            if distance >= Config.Locations[k]["radius"] and listen and currentShop == k then
+                listen = false
+            end
+
+            if distance <= Config.Locations[k]["radius"] and listen then sleep = 5 end
+        end
+
+        local chipCoords = vector3(Config.SellCasinoChips["coords"]["x"], Config.SellCasinoChips["coords"]["y"], Config.SellCasinoChips["coords"]["z"])
+        local distance = #(playerCoords - chipCoords)
+        if distance <= Config.SellCasinoChips["radius"] then
+            inChips = true
+            sleep = 5
+        else
+            inChips = false
+        end
+
+        Wait( sleep )
     end
 end)
 
 RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
-    PlayerData = QBCore.Functions.GetPlayerData()
     createBlips()
     createPeds()
-    TriggerServerEvent('qb-shops:server:SetShopList')
 end)
 
 RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
     deletePeds()
-    PlayerData = nil
-end)
-
-RegisterNetEvent('QBCore:Player:SetPlayerData', function(val)
-    PlayerData = val
 end)
 
 AddEventHandler('onResourceStart', function(resourceName)
-    if GetCurrentResourceName() ~= resourceName then return end
-    createBlips()
-    createPeds()
-    TriggerServerEvent('qb-shops:server:SetShopList')
+    if GetCurrentResourceName() == resourceName then
+        createBlips()
+        createPeds()
+    end
 end)
 
 AddEventHandler('onResourceStop', function(resourceName)
-    if GetCurrentResourceName() ~= resourceName then return end
-
-    deletePeds()
-end)
-
--- Threads
-if not Config.UseTarget then
-    CreateThread(function()
-        for shop in pairs(Config.Locations) do
-            NewZones[#NewZones+1] = CircleZone:Create(vector3(Config.Locations[shop]["coords"]["x"], Config.Locations[shop]["coords"]["y"], Config.Locations[shop]["coords"]["z"]), Config.Locations[shop]["radius"], {
-                useZ = true,
-                debugPoly = false,
-                name = shop,
-            })
-        end
-
-        local combo = ComboZone:Create(NewZones, {name = "RandomZOneName", debugPoly = false})
-        combo:onPlayerInOut(function(isPointInside, _, zone)
-            if isPointInside then
-                currentShop = zone.name
-                TriggerServerEvent('qb-shops:server:SetShopList')
-                currentData = Config.Locations[zone.name]
-                exports["qb-core"]:DrawText(Lang:t("info.open_shop"))
-                listenForControl()
-            else
-                exports["qb-core"]:HideText()
-                listen = false
-            end
-        end)
-
-        local sellChips = CircleZone:Create(vector3(Config.SellCasinoChips.coords["x"], Config.SellCasinoChips.coords["y"], Config.SellCasinoChips.coords["z"]), Config.SellCasinoChips.radius, {useZ = true})
-        sellChips:onPlayerInOut(function(isPointInside)
-            if isPointInside then
-                inChips = true
-                exports["qb-core"]:DrawText(Lang:t("info.sell_chips"))
-                listenForControl()
-            else
-                inChips = false
-                exports["qb-core"]:HideText()
-            end
-        end)
-    end)
-end
-
-CreateThread(function()
-    for k1, v in pairs(Config.Locations) do
-        if v.requiredJob and next(v.requiredJob) then
-            for k in pairs(v.requiredJob) do
-                Config.Locations[k1].requiredJob[k] = 0
-            end
-        end
-        if v.requiredGang and next(v.requiredGang) then
-            for k in pairs(v.requiredGang) do
-                Config.Locations[k1].requiredGang[k] = 0
-            end
-        end
+    if GetCurrentResourceName() == resourceName then
+        deletePeds()
     end
 end)
--- server/main.lua
+++ server/main.lua
@@ -1,72 +1,50 @@
-local QBCore = exports['qb-core']:GetCoreObject()
---Events
-QBCore.Functions.CreateCallback('qb-shops:server:SetShopInv', function(_,cb)
-    local shopInvJson = LoadResourceFile(GetCurrentResourceName(), Config.ShopsInvJsonFile)
-    cb(shopInvJson)
-end)
-RegisterNetEvent('qb-shops:server:SaveShopInv',function()
-    if not Config.UseTruckerJob then return end
-    local shopinv = {}
-    for k, v in pairs(Config.Locations) do
-        shopinv[k] = {}
-        shopinv[k].products = {}
-        for kk, vv in pairs(v.products) do
-            shopinv[k].products[kk] = {}
-            shopinv[k].products[kk].amount = vv['amount']
-        end
-    end
-    SaveResourceFile(GetCurrentResourceName(), Config.ShopsInvJsonFile, json.encode(shopinv))
-end)
+local LS_CORE = exports['ls-core']:GetCoreObject()
+
 RegisterNetEvent('qb-shops:server:UpdateShopItems', function(shop, itemData, amount)
-    if not Config.UseTruckerJob then return end
-    if not shop or not itemData or not amount then return end
-    Config.Locations[shop].products[itemData.slot].amount -= amount
-    if Config.Locations[shop].products[itemData.slot].amount < 0 then
-        Config.Locations[shop].products[itemData.slot].amount = 0
+    Config.Locations[shop]["products"][itemData.slot].amount = Config.Locations[shop]["products"][itemData.slot].amount - amount
+    if Config.Locations[shop]["products"][itemData.slot].amount <= 0 then
+        Config.Locations[shop]["products"][itemData.slot].amount = 0
     end
-    TriggerEvent('qb-shops:server:SaveShopInv')
-    TriggerClientEvent('qb-shops:client:SetShopItems', -1, shop, Config.Locations[shop].products)
+    TriggerClientEvent('qb-shops:client:SetShopItems', -1, shop, Config.Locations[shop]["products"])
 end)
+
 RegisterNetEvent('qb-shops:server:RestockShopItems', function(shop)
-    if not shop or not Config.Locations[shop].products then return end
-    local randAmount = math.random(10, 50)
-    for k in pairs(Config.Locations[shop].products) do
-        Config.Locations[shop].products[k].amount += randAmount
+    if Config.Locations[shop]["products"] ~= nil then
+        local randAmount = math.random(10, 50)
+        for k in pairs(Config.Locations[shop]["products"]) do
+            Config.Locations[shop]["products"][k].amount = Config.Locations[shop]["products"][k].amount + randAmount
+        end
+        TriggerClientEvent('qb-shops:client:RestockShopItems', -1, shop, randAmount)
     end
-    TriggerEvent('qb-shops:server:SaveShopInv')
-    TriggerClientEvent('qb-shops:client:RestockShopItems', -1, shop, randAmount)
 end)
+
+LS_CORE.Callback.Functions.CreateCallback('qb-shops:server:getLicenseStatus', function(source, cb)
+    local src = source
+    local Player = LS_CORE.Functions.GetPlayer(src)
+    local licenseItem = Player.Functions.GetItem("weaponlicense")
+    cb(licenseItem)
+end)
+
 local ItemList = {
     ["casinochips"] = 1,
 }
+
 RegisterNetEvent('qb-shops:server:sellChips', function()
     local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    local xItem = Player.Functions.GetItemByName("casinochips")
-    if xItem then
-        for k in pairs(Player.PlayerData.items) do
-            if Player.PlayerData.items[k] then
-                if ItemList[Player.PlayerData.items[k].name] then
-                    local price = ItemList[Player.PlayerData.items[k].name] * Player.PlayerData.items[k].amount
-                    Player.Functions.RemoveItem(Player.PlayerData.items[k].name, Player.PlayerData.items[k].amount, k)
+    local Player = LS_CORE.Functions.GetPlayer(src)
+    local xItem = Player.Functions.GetItem("casinochips")
+    if xItem ~= nil then
+        for k in pairs(Player.DATA.items) do
+            if Player.DATA.items[k] ~= nil then
+                if ItemList[Player.DATA.items[k].name] ~= nil then
+                    local price = ItemList[Player.DATA.items[k].name] * Player.DATA.items[k].amount
+                    Player.Functions.RemoveItem(Player.DATA.items[k].name, Player.DATA.items[k].amount, k)
+                    
                     Player.Functions.AddMoney("cash", price, "sold-casino-chips")
-                    QBCore.Functions.Notify(src, "You sold your chips for $" .. price)
+                    TriggerClientEvent('QBCore:Notify', src, "You sold your chips for $" .. price)
                     TriggerEvent("qb-log:server:CreateLog", "casino", "Chips", "blue", "**" .. GetPlayerName(src) .. "** got $" .. price .. " for selling the Chips")
                 end
             end
         end
-    else
-        QBCore.Functions.Notify(src, "You have no chips..")
     end
-end)
-RegisterNetEvent('qb-shops:server:SetShopList',function()
-    local shoplist = {}
-    local cnt = 0
-    for k, v in pairs(Config.Locations) do
-        cnt = cnt + 1
-        shoplist[cnt] = {}
-        shoplist[cnt].name = k
-        shoplist[cnt].coords = v.delivery
-    end
-    TriggerClientEvent('qb-truckerjob:client:SetShopList',-1,shoplist)
 end)
